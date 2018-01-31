69eb4b67a7373b2da008cb59cfda87e5264917c9
Zack Doherty <zack.doherty@gotinder.com>
2018-01-31 01:30:27 -0800

    nodejs!

--- hack/scripts/setup-latex.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# what: sets up a latex compiler, this make take some time to run..
-#
-# we'll need some packages!
-sudo apt install -y texlive-base
-# and that's it, fortunately
-
---
+++ hack/scripts/setup-nodejs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# what: installs nodejs and the npm package manager
+#
+sudo apt install -y nodejs npm
+
