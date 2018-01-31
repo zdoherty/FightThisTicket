2ef77d5a53d042167f92a1f6b4857165f7a5ef17
Zack Doherty <zack.doherty@gotinder.com>
2018-01-31 02:07:01 -0800

    better nodejs setup script

--- hack/scripts/setup-nodejs.sh
+++ hack/scripts/setup-nodejs.sh
@@ -2,5 +2,19 @@
 #
 # what: installs nodejs and the npm package manager
 #
-sudo apt-get install -y nodejs npm
+# the packages are distributed from an https source, so we'll need that transport
+sudo apt-get install -y apt-transport-https
+
+# and they're signed! nice!
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+
+NODEREPO="node_6.x"
+DISTRO="xenial"
+
+echo "deb https://deb.nodesource.com/${NODEREPO} ${DISTRO} main" > /etc/apt/sources.list.d/nodesource.list
+echo "deb-src https://deb.nodesource.com/${NODEREPO} ${DISTRO} main" >> /etc/apt/sources.list.d/nodesource.list
+
+# since we're adding new repos, we need to update
+sudo apt-get update
+sudo apt-get install -y nodejs
 
